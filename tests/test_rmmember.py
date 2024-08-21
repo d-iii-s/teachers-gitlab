a1188712da0b75973c317017aa3d8b39ae00a287
Ondřej Tulach <ondrej.tulach@gmail.com>
2024-08-21 16:52:49 +0200

    More tests remove-member

--- tests/test_rmmember.py
+++ tests/test_rmmember.py
@@ -2,7 +2,7 @@ import logging
 
 import teachers_gitlab.main as tg
 
-def test_remove_member(mock_gitlab):
+def test_remove_member_that_doesnt_exist(mock_gitlab):
     entries = [
         {'login': 'alpha'},
     ]
@@ -28,11 +28,80 @@ def test_remove_member(mock_gitlab):
         }]
     )
 
+    mock_gitlab.on_api_get(
+        'projects/42/members/5',
+        response_404=True
+    )
+
+    mock_gitlab.on_api_get(
+        'users?username=alpha',
+        response_json=[
+            {
+                'id': 5,
+                'username': 'alpha',
+            }
+        ]
+    )
+
+    mock_gitlab.report_unknown()
+
     tg.action_remove_member(
         mock_gitlab.get_python_gitlab(),
         logging.getLogger("removemember"),
         tg.ActionEntries(entries),
-        1,
+        'login',
+        False,
+        'student/{login}',
+    )
+
+def test_remove_member(mock_gitlab):
+    entries = [
+        {'login': 'mario'},
+    ]
+
+    mock_gitlab.register_project(42, 'student/mario', members =[
+        {
+            "id": 2157753,
+            "username": "mario",
+            "name": "xxx",
+            "state": "active",
+            "web_url": "https://gitlab.com/mario",
+            "access_level": 50,
+            "membership_state": "active"
+        },
+        {
+            "id": 834534,
+            "username": "sonic",
+            "name": "xxx",
+            "state": "active",
+            "web_url": "https://gitlab.com/sonic",
+            "access_level": 50,
+            "membership_state": "active"
+        }]
+    )
+
+    mock_gitlab.on_api_get(
+        'projects/42/members/2157753',
+        response_404=True
+    )
+
+    mock_gitlab.on_api_get(
+        'users?username=mario',
+        response_json=[
+            {
+                'id': 2157753,
+                'username': 'mario',
+            }
+        ]
+    )
+
+    mock_gitlab.report_unknown()
+
+    tg.action_remove_member(
+        mock_gitlab.get_python_gitlab(),
+        logging.getLogger("removemember"),
+        tg.ActionEntries(entries),
+        'login',
         False,
         'student/{login}',
     )
